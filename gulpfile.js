9d5716fac4d89e08b90023577e6b0daae60aa195
Victor Diaz de Leon <vrdiazdel@gmail.com>
2016-01-12 15:47:16 -0800

    Update gulpfile.js
    Updated gulp.watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -35,8 +35,8 @@ gulp.task('images', function() {
 
 // Watch task
 gulp.task('watch', function() {
-	gulp.watch('./src/js/*.js', ['scripts']);
-	gulp.watch('./src/less/*.less', ['styles']);
+	gulp.watch('./src/js/**/.js', ['scripts']);
+	gulp.watch('./src/less/**/.less', ['styles']);
 	gulp.watch('./src/img/*', ['images']);
 });
 
